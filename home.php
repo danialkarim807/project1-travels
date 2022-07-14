6034e3b045ec2180fa5a483e6125f7fc2896ad3d
danialkarim807 <danialkarim8@gmail.com>
2022-07-14 20:33:46 +0500

    first commit

---
+++ home.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <!-- custome awiper link -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+    <!-- font owsome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <!-- custome css kink -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <!-- header session starts -->
+    <section class="header">
+        <a href="home.php" class="logo">Travel</a>
+
+        <nav class="navbar">
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="pacakge.php">Pacakge</a>
+            <a href="book.php">Book</a>
+        </nav>
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+    </section>
+    <!-- header session endses -->
+
+
+
+
+
+
+
+    <!-- /////////////////////////footer section start.................. -->
+
+    <section class="footer">
+        <div class="box-container">
+            <div class="box">
+                <h3>Quick links</h3>
+                <a href="home.php"><i class="fas fa-angle-right"></i>Home</a>
+                <a href="about.php"><i class="fas fa-angle-right"></i>About</a>
+                <a href="pacakge.php"><i class="fas fa-angle-right"></i>Pacakge</a>
+                <a href="book.php"><i class="fas fa-angle-right"></i>Book</a>
+            </div>
+            <div class="box">
+                <h3>Quick links</h3>
+                <a href="home.php"><i class="fas fa-angle-right"></i>Home</a>
+                <a href="about.php"><i class="fas fa-angle-right"></i>About</a>
+                <a href="pacakge.php"><i class="fas fa-angle-right"></i>Pacakge</a>
+                <a href="book.php"><i class="fas fa-angle-right"></i>Book</a>
+            </div>
+            <div class="box">
+                <h3>Quick links</h3>
+                <a href="home.php"><i class="fas fa-angle-right"></i>Home</a>
+                <a href="about.php"><i class="fas fa-angle-right"></i>About</a>
+                <a href="pacakge.php"><i class="fas fa-angle-right"></i>Pacakge</a>
+                <a href="book.php"><i class="fas fa-angle-right"></i>Book</a>
+            </div>
+            <div class="box">
+                <h3>Quick links</h3>
+                <a href="home.php"><i class="fas fa-angle-right"></i>Home</a>
+                <a href="about.php"><i class="fas fa-angle-right"></i>About</a>
+                <a href="pacakge.php"><i class="fas fa-angle-right"></i>Pacakge</a>
+                <a href="book.php"><i class="fas fa-angle-right"></i>Book</a>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+
+
+    <!-- /////////////////footer section  end................ -->
+
+
+
+
+
+
+
+
+
+
+
+    <!-- swiper js link -->
+    <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+    <!-- js script link -->
+    <script src="/script.js"></script>
+
+</body>
+
+</html>
